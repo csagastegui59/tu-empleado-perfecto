3c06f0c10a6b5a810a686137c874d90afd388110
csagastegui59 <csagastegui59@gmail.com>
2022-06-03 08:46:07 -0500

    modify root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,22 @@
 import { Module } from '@nestjs/common';
+import { CompaniesModule } from './companies/companies.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [],
+  imports: [
+    EmployeesModule,
+    CompaniesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'tu-empleado-perfecto',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
