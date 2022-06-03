e3a87204dbba5bb5eded744211c2eaefb4a43860
csagastegui59 <csagastegui59@gmail.com>
2022-06-03 08:47:45 -0500

    add companies service

---
+++ src/companies/companies.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateEmployeeDto } from '../employees/dto/create-employee.dto';
+import { Employee } from '../employees/employees.entity';
+import { EmployeesRepository } from '../employees/employees.repository';
+import { CompaniesRepository } from './companies.repository';
+import { Company } from './company.entity';
+import { CreateCompanyDto } from './dto/create-company.dto';
+
+@Injectable()
+export class CompaniesService {
+  constructor(
+    @InjectRepository(CompaniesRepository)
+    private companiesRepository: CompaniesRepository,
+    @InjectRepository(EmployeesRepository)
+    private employeesRepository: EmployeesRepository,
+  ) {}
+  getAll(): Promise<Company[]> {
+    return this.companiesRepository.getAll();
+  }
+
+  getOneById(id: string): Promise<Company> {
+    return this.companiesRepository.getOneById(id);
+  }
+
+  createOne(createCompanyDto: CreateCompanyDto): Promise<Company> {
+    return this.companiesRepository.createOne(createCompanyDto);
+  }
+
+  async addEmployee(createEmployeeDto: CreateEmployeeDto): Promise<Employee> {
+    const company = await this.companiesRepository.getOneById(
+      createEmployeeDto.companyId,
+    );
+
+    const { email, name, rut } = createEmployeeDto;
+    const employee = new Employee();
+    employee.company = company;
+    employee.email = email;
+    employee.name = name;
+    employee.rut = rut;
+
+    return this.employeesRepository.save(employee);
+  }
+}
